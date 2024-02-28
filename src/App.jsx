cf8e94809a98db5ee22af193d52d5a31d2315c21
Emilio Pernia <emilio.pernia@invow.com>
2024-02-28 11:40:39 -0300

    create sections and component nav

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { Nav } from "./components";
+import { CustomerReviews, Footer, Hero, PopularProduct, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+
+const App = () => (
+    <main className="relative">
+        <nav>
+            <Nav />
+            <section className="xl:p-1 wide:padding-r padding-b">
+                <Hero />
+            </section>
+            <section className="padding">
+                <PopularProduct />
+            </section>
+            <section className="padding">
+                <SuperQuality />
+            </section>
+            <section className="padding-x py-10">
+                <Services />
+            </section>
+            <section className="padding">
+                <SpecialOffer />
+            </section>
+            <section className="bg-pale-blue padding">
+                <CustomerReviews />
+            </section>
+            <section className="padding-x sm:py-32 py-16 w-full">
+                <Subscribe />
+            </section>
+            <section className="bg-black padding-x padding-t pb-8">
+                <Footer />
+            </section>
+        </nav>
+    </main>
+);
+
+export default App;
